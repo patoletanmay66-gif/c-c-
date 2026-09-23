1a4b264541f392c1c02aa294bce88c57efb82b52
patoletanmay66-gif <patoletanmay66@gmail.com>
2026-09-23 22:42:39 +0530

    Add files via upload

---
+++ int cube.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int h;
+    printf("Enter the number to be cubed:");
+    scanf("%d",&h);
+    int b=h*h*h;
+    printf("The cube of the number is: %d",b);
+    
+
+}
---
+++ int square.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    int a;
+    printf("Enter the number to be squared:");
+    scanf("%d",&a);
+    int b=a*a;
+    printf("The square of the number is: %d",b);
+    
+}
